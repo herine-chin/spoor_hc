b04fb27f1e587732565813af26afba994a672121
Herine Chin <herine.chin@gmail.com>
2014-12-18 11:33:25 -0800

    add create note form and save to db

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,18 @@
 class NotesController < ApplicationController
 
   def index
+    @note = Note.new
+  end
+
+  def create
+    Note.create(note_params)
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:note_message)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217000049) do
+ActiveRecord::Schema.define(version: 20141218190316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: true do |t|
+    t.string   "note_message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
